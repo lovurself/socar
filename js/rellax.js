6f2e376914e489f80d5cd254baa9c751a8cac285
lovurself <lovurself_44@naver.com>
2022-04-29 23:18:58 +0900

    card 섹션 반응형 완성

--- js/rellax.js
+++ js/rellax.js
@@ -1,7 +1,1 @@
 const rellax = new Rellax('.rellax');
-const rellaxItem = document.querySelector(".card_txt");
-const media = matchMedia("screen and (max-width: 992px)");
-
-media.addEventListener(() => {
-    rellaxItem.classList.remove(rellax);
-});
